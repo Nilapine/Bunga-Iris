0f4c6c5453726c7dfc0c523e7be7f227dc35a803
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-22 08:26:47 +0700

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -1,6 +1,25 @@
 import streamlit as st
 from web_functions import load_data
 
+# CSS untuk memodifikasi posisi elemen di sidebar
+def center_sidebar_menu():
+    st.markdown(
+        """
+        <style>
+        /* Memusatkan elemen sidebar */
+        section[data-testid="stSidebar"] .css-ng1t4o { 
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+# Terapkan CSS untuk sidebar
+center_sidebar_menu()
+
 def app(df,x,y):
     # Judul Halaman Aplikasi
     st.title("Aplikasi Prediksi Jenis Tanaman Iris")
