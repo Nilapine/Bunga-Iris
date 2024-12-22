9fc9cf6fdef12f03ebf5b4b5dbc0f1d1458e09e3
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-22 10:04:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,22 @@ col1, col2 = title_container.columns([1, 5])
 with col1:
     st.image("https://png.pngtree.com/png-vector/20240528/ourmid/pngtree-blue-and-purple-iris-flower-png-image_12520393.png", width=100)
 with col2:
-    st.markdown("<style>div[role='tablist']{justify-content: center;}</style>", unsafe_allow_html=True)  #Align the tabs to the right
+    # Align tabs in the center and add margin to increase spacing between menu items
+    st.markdown("""
+    <style>
+        div[role='tablist'] {
+            justify-content: center;
+            margin-bottom: 20px;  /* Increase spacing between the tabs */
+        }
+        div[role='tablist'] > div {
+            margin-right: 20px;  /* Increase space between individual tabs */
+        }
+        .css-ffhzg2 {
+            margin-bottom: 40px;  /* Additional bottom margin if needed */
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    
     selected_tab = st.tabs(list(Tabs.keys()))  #Use st.tabs for the tab menu
 
 # Kondisi untuk menjalankan fungsi app di setiap tab
