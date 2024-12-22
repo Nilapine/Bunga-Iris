1bacd0bd57d188e4e5770722fbad9a48e92b4fe4
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-22 09:40:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import streamlit as st
 from web_functions import load_data
 from Tabs import home, predict, visualise
 
+#sidebar menu
+Tabs = {
+    "Home": home,
+    "Prediction" : predict,
+    "Visualisation" : visualise,
+}
 
 #load dataset
 df,x,y = load_data()
@@ -13,16 +19,6 @@ with col1:
     st.image("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", width=100)
 with col2:
     selected_tab = st.tabs(list(Tabs.keys())) # Tabs are now within the second column
-
-#sidebar menu
-Tabs = {
-    "Home": home,
-    "Prediction" : predict,
-    "Visualisation" : visualise,
-}
-
-# Membuat tab horizontal
-selected_tab = st.tabs(list(Tabs.keys()))
 
 # Kondisi untuk menjalankan fungsi app di setiap tab
 for i, tab in enumerate(selected_tab):
