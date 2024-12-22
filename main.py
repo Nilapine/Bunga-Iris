73109a6a3d9eddc1e6e0b5d529a5df523f8069f6
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-22 09:17:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,45 @@ from Tabs import home, predict, visualise
 from streamlit_navigation_bar import st_navbar
 import pages as pg
 
+# *** IMPORTED NAVIGATION BAR
+st.set_page_config(initial_sidebar_state="collapsed")
+pages = ["Home", "Prediction", "Visualisation"]
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(
+    parent_dir, "https://daylily-phlox.eu/wp-content/uploads/2023/10/Iris-virginica-Pond-Crown-Point.jpg")
+styles = {
+    "nav": {
+        "background-color": "#5D7298",
+        "justify-content": "left",
+    },
+    "img": {
+        "padding-right": "14px",
+        "width": "150px",
+        "height": "150px"
+
+    },
+    "span": {
+        "color": "white",
+        "padding": "14px",
+    },
+    "active": {
+        "background-color": "#A6B2C9",
+        "color": "var(--text-color)",
+        "font-weight": "normal",
+        "padding": "14px",
+    }
+}
+options = {
+    "show_menu": False,
+    "show_sidebar": False,
+}
+
+Tabs = st_navbar(
+    pages,
+    logo_path=logo_path,
+    styles=styles,
+    options=options,
+)
 
 #sidebar menu
 Tabs = {
