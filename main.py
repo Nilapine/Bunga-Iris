0cf41639c85a5d51e0b09eedb38daa0d9e5d3521
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-22 09:28:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ Tabs = {
     "Visualisation" : visualise,
 }
 
-#membuat radio option
-#page = st.sidebar.radio("Pages",list(Tabs.keys()))
+#load dataset
+df,x,y = load_data()
+
+# Header dengan logo dan judul
+title_container = st.container()
+col1, col2 = title_container.columns([1, 5])
+with col1:
+    st.image("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", width=100)
+with col2:
+    st.markdown("<h1 style='text-align: right;'>Navigasi</h1>", unsafe_allow_html=True)
 
 # Membuat tab horizontal
 selected_tab = st.tabs(list(Tabs.keys()))
-
-#load dataset
-df,x,y = load_data()
 
 # Kondisi untuk menjalankan fungsi app di setiap tab
 for i, tab in enumerate(selected_tab):
