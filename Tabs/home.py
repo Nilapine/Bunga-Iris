4c36e73792e65abe842b35cb8d6230701c6d5788
bos ketcielz <113289837+Nilapine@users.noreply.github.com>
2024-12-31 05:47:20 +0700

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -5,7 +5,7 @@ def app(df, x, y):
     # Add a large horizontal image of the Iris flower at the bottom of the page
     st.image(
         'https://asset.kompas.com/crops/pBgd0CVrw5satlJj1P_ud_0EAoM=/0x0:1000x667/1200x800/data/photo/2022/10/30/635db6ae22aa4.jpg',  # Replace with your actual large horizontal iris image URL
-        caption="Iris Flower - Horizontal",
+        caption="Iris Flower",
         use_column_width=True  # Make the image stretch across the full width of the page
     )
     
@@ -22,14 +22,14 @@ def app(df, x, y):
     )
     
     # Add a title for the application
-    st.title("Aplikasi Prediksi Jenis Tanaman Iris")
+    #st.title("Aplikasi Prediksi Jenis Tanaman Iris")
     
     # Display iris flower images in columns (side by side) with same size
     col1, col2, col3 = st.columns(3)
     
     # Set a fixed size for all images to make them uniform
-    image_width = 350  # Fixed width for each image
-    image_height = 350  # Fixed height for each image
+    #image_width = 350  # Fixed width for each image
+    #image_height = 350  # Fixed height for each image
     
     with col1:
         st.image('https://images.squarespace-cdn.com/content/v1/61eeea89d60f57793d9e114b/1706854176756-Y4XKV9Q0OQ5F2C0ICPDI/iris%2Bsetosa%2B%25282%2529.jpg?format=1000w', caption="Iris Setosa", width=image_width, height=image_height)
